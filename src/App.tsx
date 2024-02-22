96648d0e82260cb2cb635c7a64511082768e1869
agodse21 <agodse9@gmail.com>
2024-02-22 21:44:12 +0530

    feat:app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-
+import { useState } from "react";
+import { HiMenuAlt2 } from "react-icons/hi";
+import './App.css';
+import Home from "./components/home";
+import Navbar from "./components/navbar";
 
 function App() {
- 
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-   <div>
-    <h1 className="text-red-800">set up</h1>
-    <nav>oky</nav>
-    <h1>heading</h1>
-    <div>home</div>
-    <div>about</div>
-   </div>
+    <div className="relative" style={{
+      fontFamily: "Poppins",
+
+    }}>
+      <button className="absolute top-10 left-12 z-10" aria-label='Open Navbar' style={{
+        backgroundColor: "transparent"
+      }} onClick={() => { setIsOpen(!isOpen) }}  >
+        <HiMenuAlt2 color={"#06223f"} className="text-[40px] font-bold" />
+      </button>
+
+      <Navbar isOpen={isOpen} />
+
+      <Home />
+
+    </div>
   )
 }
 
