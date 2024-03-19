35059373e1c48cb670e934faaa688b459a009c96
agodse21 <agodse9@gmail.com>
2024-03-19 23:06:50 +0530

    feat:utils for portfolio

---
+++ src/lib/utils.ts
@@ -0,0 +1,111 @@
+export const navLinks = [
+  {
+    lebal: "home",
+    path: "#home",
+  },
+  {
+    lebal: "about",
+    path: "#about",
+  },
+  {
+    lebal: "projects",
+    path: "#projects",
+  },
+  {
+    lebal: "tech stack",
+    path: "#tech-stack",
+  },
+  {
+    lebal: "skills",
+    path: "#skills",
+  },
+  {
+    lebal: "contact",
+    path: "#contact",
+  },
+];
+
+export const typeWritterStrings = [
+  "Frontend developer.",
+  "MERN Stack Developer.",
+  "Full Stack web developer.",
+  "Tech enthusiastic.",
+];
+
+export const skillsArray = [
+  {
+    lebal: "HTML",
+  },
+  {
+    lebal: "CSS",
+  },
+  {
+    lebal: "JavaScript",
+  },
+  {
+    lebal: "TypeScript",
+  },
+  {
+    lebal: "React.js",
+  },
+  {
+    lebal: "Node.js",
+  },
+  {
+    lebal: "Express.js",
+  },
+  {
+    lebal: "MongoDB",
+  },
+];
+
+export const WorkExprienceArray = [
+  {
+    company: "Apxor",
+    position: "Associate SDE",
+    desc: "Implemented new features, worked on improving the product exprience,system designing.Technologies: React, TypeScript, GraphQL",
+    start_date: "Apr 2023",
+    end_date: "Present",
+    badge: "Full Time",
+  },
+  {
+    company: "Masai",
+    position: "Trainee",
+    desc: "As a Trainee, I Learned MERN stack, honing my skills through hands-on experience. During this time, I successfully developed multiple projects, gaining practical knowledge and expertise in web development.",
+    start_date: "Apr 2022",
+    end_date: "Dec 2022",
+    badge: "Internship",
+  },
+];
+
+export const TestimonialsArray = [
+  {
+    avatar:
+      "https://media.licdn.com/dms/image/D5603AQGv6ZmKaeq96A/profile-displayphoto-shrink_100_100/0/1675263176621?e=1714608000&v=beta&t=4EvmHhgbQ3h5so4myttYpeTn7SGrA2czurKxfGwztKE",
+    name: "Chandra Shekar",
+    testimonial:
+      "I'm amazed by Amol's ability to MERN Stack, this skill he uses to his benefit when making projects in Masai He is a highly focused person as well as analytical and can add to any team he is a part of. His energy to make things happen was contagious and it helped us achieve great goals. One of the best analysts we had for sure.",
+  },
+  {
+    avatar:
+      "https://media.licdn.com/dms/image/D4D35AQHeDCcFGIfcpw/profile-framedphoto-shrink_100_100/0/1667103899267?e=1709445600&v=beta&t=6HUR3tcS72UzCnq63A2Z8pyQ0gWpdntpKgdYUR__ulA",
+    name: "Vivek Rajput",
+    testimonial:
+      "Amol is a highly energetic and self-motivated person with good communication skills. we worked on a couple of projects together and he has excellent interpersonal skills. I got stunned by his MERN, javascript, HTML, and CSS skills. I highly recommend Amol and love to work with him again.",
+  },
+  {
+    avatar:
+      "https://media.licdn.com/dms/image/C4E03AQHLVpr2p84t0Q/profile-displayphoto-shrink_100_100/0/1649060633250?e=1714608000&v=beta&t=MlARg4JXvIQxGNX3mwq1vDg8sp4v2jR1p4A4Oh79v9g",
+    name: "Shilpa Shingapure",
+    testimonial:
+      "Amol is an amazing person, who brings all of his skills to projects effectively. he's very good at the front end and also the best in guiding his teammates.",
+  },
+
+  {
+    avatar:
+      "https://media.licdn.com/dms/image/C4D03AQFJtg5OSMUvIw/profile-displayphoto-shrink_100_100/0/1657434493077?e=1714608000&v=beta&t=qxlSL6jGnswYcpmp0v_xgMJBbkxd0c_9kVX5VBrpITU",
+    name: "NileshSingh Chauhan",
+    testimonial:
+      "I highly recommend Amol as a MERN- stack developer and would love to work together again. He is amazing at his logical and creative thinking !",
+  },
+];
